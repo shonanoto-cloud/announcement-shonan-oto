65ea2e6b1e1c4e23912d77fab04ada0afcc2b916
shonan_oto <shonanoto@gmail.com>
2026-04-18 22:55:29 +0900

    image test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import image from '@astrojs/image';
+import sitemap from "@astrojs/sitemap"
+import image from '@astrojs/image';;
 import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
 	site: "https://example.com",
